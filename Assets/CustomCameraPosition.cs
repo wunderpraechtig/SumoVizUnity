9a4e032b3ca32798445759703452fe37752bfe00
Florian Sesser <fs@accu-rate.de>
2015-06-02 17:59:20 +0200

    Custom camera position
    das hat gedauert bis ich das ma hatte, wa

--- Assets/CustomCameraPosition.cs
+++ Assets/CustomCameraPosition.cs
@@ -5,25 +5,18 @@ public class CustomCameraPosition : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-		//Camera.current.transform.position += new Vector3(0, -10, 10);
-		//transform.position += transform.
-		// transform.position += new Vector3(0, 10, -10);
-
-		//TeleportRandomly();
 		InvokeRepeating("TeleportRandomly", 2, 1);
-
-
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		// Camera.current.transform.position += new Vector3(0, -1, 1);
 	}
 
 	public void TeleportRandomly() {
 		Vector3 direction = Random.onUnitSphere;
 		direction.y = Mathf.Clamp(direction.y, 0.5f, 1f);
-		float distance = 2 * Random.value + 1.5f;
+		float distance = 2 * Random.value + 15f;
 		transform.localPosition = direction * distance;
+		transform.position = direction * distance;
 	}
 }
