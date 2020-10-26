4f84fac90af54fc0623133e6600e5510452a3c06
Marvin Weisbrod <marvinweisbrod@gmail.com>
2020-10-26 19:36:27 +0100

    fixed 2d ui layout and buttons

--- Assets/Scripts/MainMenuToggle.cs
+++ Assets/Scripts/MainMenuToggle.cs
@@ -2,16 +2,17 @@
 
 public class MainMenuToggle : MonoBehaviour
 {
-    [SerializeField] MainMenuLogic mainMenuLogic = null;
+    //[SerializeField] MainMenuLogic mainMenuLogic = null;
 
     private void Awake() {
     }
 
-    //private void Update() {
+    //private void Update()
+    //{
     //    if (/*some key pressed?*/)
     //    {
     //        mainMenuLogic.ToggleMainMenu();
     //    }
     //}
-    
+
 }
